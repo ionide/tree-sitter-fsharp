dc5145f1a079a87d79e3ca50de9c5171035921da
Joseph Price <pricejosephd@gmail.com>
2024-08-13 13:21:56 -0400

    handle anon record types

--- test/highlight/type_definitions.fsx
+++ test/highlight/type_definitions.fsx
@@ -5,3 +5,33 @@ type A() =
 //^ keyword.function
 //       ^ variable.parameter.builtin
 //            ^ function.method
+
+let x = ResizeArray<string>()
+//<- keyword.function
+//  ^ variable
+//      ^ function.call
+//                 ^ operator
+//                  ^ type.builtin
+//                        ^ operator
+//                         ^ variable
+//                          ^ variable
+
+let x = ResizeArray<A>()
+//<- keyword.function
+//  ^ variable
+//      ^ function.call
+//                 ^ operator
+//                  ^ type
+//                   ^ operator
+//                    ^ variable
+//                     ^ variable
+
+let x = ResizeArray<{|word: string |}>()
+//<- keyword.function
+//  ^ variable
+//      ^ function.call
+//                 ^ operator
+//                  ^ punctuation.bracket
+//                   ^ punctuation.bracket
+//                    ^ property
+//                          ^ type.builtin
