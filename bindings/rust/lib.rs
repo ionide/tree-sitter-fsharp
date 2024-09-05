e0ab53ddc7961c8a14deca2fe7cba64f31d6474d
Nikolaj Sidorenco <Nikolaj176@hotmail.com>
2024-09-05 14:52:08 +0200

    feat: update tree-sitter (#91)

--- bindings/rust/lib.rs
+++ bindings/rust/lib.rs
@@ -1,69 +1,48 @@
-
-//! This crate provides FSharp language support for the [tree-sitter][] parsing
-//! library. There are separate languages for implementation, (`.fs`),
-//! signature (`.fsi`).
+//! This crate provides Fsharp language support for the [tree-sitter][] parsing library.
 //!
-//! Typically, you will use the [language_fsharp][language func] function to add
-//! this language to a tree-sitter [Parser][], and then use the parser to parse
-//! some code:
+//! Typically, you will use the [language][language func] function to add this language to a
+//! tree-sitter [Parser][], and then use the parser to parse some code:
 //!
 //! ```
 //! let code = r#"
-//!   module M =
-//!     let x = 0
 //! "#;
 //! let mut parser = tree_sitter::Parser::new();
+//! let language = tree_sitter_fsharp::LANGUAGE_FSHARP;
 //! parser
-//!     .set_language(&tree_sitter_fsharp::language_fsharp())
-//!     .expect("Error loading FSharp grammar");
+//!     .set_language(&language.into())
+//!     .expect("Error loading Fsharp parser");
 //! let tree = parser.parse(code, None).unwrap();
 //! assert!(!tree.root_node().has_error());
 //! ```
 //!
 //! [Language]: https://docs.rs/tree-sitter/*/tree_sitter/struct.Language.html
-//! [language func]: fn.language_fsharp.html
+//! [language func]: fn.language.html
 //! [Parser]: https://docs.rs/tree-sitter/*/tree_sitter/struct.Parser.html
 //! [tree-sitter]: https://tree-sitter.github.io/
 
-use tree_sitter::Language;
+use tree_sitter_language::LanguageFn;
 
 extern "C" {
-    fn tree_sitter_fsharp() -> Language;
-    fn tree_sitter_fsharp_signature() -> Language;
+    fn tree_sitter_fsharp() -> *const ();
+    fn tree_sitter_fsharp_signature() -> *const ();
 }
 
-/// Get the tree-sitter [Language][] for FSharp.
-///
-/// [Language]: https://docs.rs/tree-sitter/*/tree_sitter/struct.Language.html
-pub fn language_fsharp() -> Language {
-    unsafe { tree_sitter_fsharp() }
-}
+/// The tree-sitter [`LanguageFn`] for this grammar.
+pub const LANGUAGE_FSHARP: LanguageFn = unsafe { LanguageFn::from_raw(tree_sitter_fsharp) };
+pub const LANGUAGE_SIGNATURE: LanguageFn = unsafe { LanguageFn::from_raw(tree_sitter_fsharp_signature) };
 
-/// Get the tree-sitter [Language][] for FSharp signature.
-///
-/// [Language]: https://docs.rs/tree-sitter/*/tree_sitter/struct.Language.html
-pub fn language_fsharp_signature() -> Language {
-    unsafe { tree_sitter_fsharp_signature() }
-}
-
-/// The content of the [`node-types.json`][] file for FSharp.
+/// The content of the [`node-types.json`][] file for this grammar.
 ///
 /// [`node-types.json`]: https://tree-sitter.github.io/tree-sitter/using-parsers#static-node-types
-pub const FSHARP_NODE_TYPES: &'static str = include_str!("../../fsharp/src/node-types.json");
+pub const FSHARP_NODE_TYPES: &str = include_str!("../../fsharp/src/node-types.json");
+pub const SIGNATURE_NODE_TYPES: &str = include_str!("../../fsharp_signature/src/node-types.json");
 
-/// The content of the [`node-types.json`][] file for FSharp signature.
-///
-/// [`node-types.json`]: https://tree-sitter.github.io/tree-sitter/using-parsers#static-node-types
-pub const SIGNATURE_NODE_TYPES: &'static str = include_str!("../../fsharp_signature/src/node-types.json");
+// NOTE: uncomment these to include any queries that this grammar contains:
 
-/// The syntax highlighting query for FSharp.
-pub const HIGHLIGHTS_QUERY: &'static str = include_str!("../../queries/highlights.scm");
-
-/// The local-variable syntax highlighting query for FSharp.
-pub const LOCALS_QUERY: &'static str = include_str!("../../queries/locals.scm");
-
-/// The symbol tagging query for FSharp.
-pub const TAGGING_QUERY: &'static str = include_str!("../../queries/tags.scm");
+pub const HIGHLIGHTS_QUERY: &str = include_str!("../../queries/highlights.scm");
+pub const INJECTIONS_QUERY: &str = include_str!("../../queries/injections.scm");
+pub const LOCALS_QUERY: &str = include_str!("../../queries/locals.scm");
+pub const TAGS_QUERY: &str = include_str!("../../queries/tags.scm");
 
 #[cfg(test)]
 mod tests {
@@ -71,8 +50,8 @@ mod tests {
     fn test_fsharp() {
         let mut parser = tree_sitter::Parser::new();
         parser
-            .set_language(&super::language_fsharp())
-            .expect("Error loading FSharp grammar");
+            .set_language(&super::LANGUAGE_FSHARP.into())
+            .expect("Error loading Fsharp parser");
 
         let code = r#"
             module M =
@@ -88,8 +67,8 @@ mod tests {
     fn test_fsharp_signature() {
         let mut parser = tree_sitter::Parser::new();
         parser
-            .set_language(&super::language_fsharp_signature())
-            .expect("Error loading FSharp signature grammar");
+            .set_language(&super::LANGUAGE_SIGNATURE.into())
+            .expect("Error loading Fsharp parser");
 
         let code = r#"
             module M =
@@ -101,4 +80,3 @@ mod tests {
         assert!(!root.has_error());
     }
 }
-
