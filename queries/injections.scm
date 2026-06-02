2ae00dd06c0f903411b08beece793b53fc0ac80c
Tuomas Hietanen <thorium@iki.fi>
2026-06-02 10:53:39 +0100

    Use text between (** and *) as markdown, to support

--- queries/injections.scm
+++ queries/injections.scm
@@ -1,7 +1,20 @@
-([
- (line_comment)
- (block_comment_content)
-] @injection.content
+;; Literate documentation comments: (** ... *)
+;; The FSharp.Formatting convention treats these as Markdown. The content node
+;; begins with the extra `*` of the `(**` opener, so we match on that to tell a
+;; doc comment apart from an ordinary `(* ... *)` block comment, and offset by
+;; one column to drop the leading `*` before handing it to Markdown.
+;; Markdown's own injections then highlight fenced code blocks (```sql, ...).
+((block_comment_content) @injection.content
+ (#match? @injection.content "^\\*")
+ (#offset! @injection.content 0 1 0 0)
+ (#set! injection.language "markdown"))
+
+;; Ordinary comments
+((block_comment_content) @injection.content
+ (#not-match? @injection.content "^\\*")
+ (#set! injection.language "comment"))
+
+((line_comment) @injection.content
  (#set! injection.language "comment"))
 
 ((xml_doc) @injection.content
